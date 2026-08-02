8b6ec9efd41ba7d1af130ccd27cd91650dd0134c
thuycq <thuycq@uel.edu.vn>
2026-08-02 18:47:35 +0700

    Simplify GreenBond26 page for st.navigation

--- app/pages/1_GreenBond26.py
+++ app/pages/1_GreenBond26.py
@@ -1,27 +1,4 @@
-import streamlit as st
-
 from app.bond_runtime import run_bond_app
 
 
 run_bond_app("greenBond26")
-
-st.markdown(
-    """
-    <style>
-        [data-testid="stSidebarNav"] ul li:first-child a {
-            font-size: 0 !important;
-        }
-
-        [data-testid="stSidebarNav"] ul li:first-child a::after {
-            content: "Demo Bond";
-            font-size: 0.95rem !important;
-        }
-
-        div[data-testid="stTabs"] div[role="tabpanel"]:first-of-type
-        [data-testid="stCaptionContainer"] {
-            display: none !important;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
